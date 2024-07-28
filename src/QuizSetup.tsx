37746dce7ac9b0e5beb98819c94f08f57091753e
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2024-07-28 18:17:32 +0100

    Added a radio input for the quiz difficulty

--- src/QuizSetup.tsx
+++ src/QuizSetup.tsx
@@ -4,25 +4,47 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Radio,
+  RadioGroup,
+  Stack,
+  HStack,
 } from "@chakra-ui/react";
+import { useState } from "react";
+
+type Difficulty = "easy" | "medium" | "hard";
 
 function QuizSetup() {
+  const [difficulty, setDifficulty] = useState<Difficulty>("easy");
+
   return (
     <>
-      <NumberInput
-        size="sm"
-        maxW={20}
-        defaultValue={10}
-        min={10}
-        max={50}
-        step={5}
-      >
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+      <HStack spacing="24px">
+        <NumberInput
+          size="sm"
+          maxW={20}
+          defaultValue={10}
+          min={10}
+          max={50}
+          step={5}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+
+        <RadioGroup
+          onChange={(nextVal: string) => setDifficulty(nextVal as Difficulty)}
+          value={difficulty}
+        >
+          <Stack direction="row">
+            <Radio value="easy">Easy</Radio>
+            <Radio value="medium">Medium</Radio>
+            <Radio value="hard">Hard</Radio>
+          </Stack>
+        </RadioGroup>
+      </HStack>
     </>
   );
 }
