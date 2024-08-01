64ab11830be22eb4b5571e1e63e09c20851f1ac6
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2024-08-01 23:55:54 +0100

    Added form labels to quiz setup

--- src/QuizSetup.tsx
+++ src/QuizSetup.tsx
@@ -7,9 +7,11 @@ import {
   Radio,
   RadioGroup,
   Stack,
-  HStack,
   Select,
   Button,
+  VStack,
+  FormControl,
+  FormLabel,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -19,6 +21,7 @@ import {
   ICategoryInfo,
   IQuestionInfo,
 } from "./types";
+import "./styles.css";
 
 function QuizSetup() {
   const [numberOfQuestions, setNumberOfQuestions] = useState<number>(10);
@@ -59,74 +62,81 @@ function QuizSetup() {
 
   return (
     <>
-      <HStack spacing="24px">
-        <NumberInput
-          size="lg"
-          maxW={32}
-          defaultValue={numberOfQuestions}
-          min={10}
-          max={50}
-          step={5}
-          onChange={(_stringVal: string, numVal: number) => {
-            setNumberOfQuestions(numVal);
-          }}
-        >
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
+      <FormControl>
+        <VStack className="quiz-setup" spacing="24px">
+          <FormLabel>Number of questions</FormLabel>
+          <NumberInput
+            size="lg"
+            maxW={32}
+            defaultValue={numberOfQuestions}
+            min={10}
+            max={50}
+            step={5}
+            onChange={(_stringVal: string, numVal: number) => {
+              setNumberOfQuestions(numVal);
+            }}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <FormLabel>Select difficulty</FormLabel>
 
-        <RadioGroup
-          onChange={(nextVal: string) => setDifficulty(nextVal as Difficulty)}
-          value={difficulty}
-        >
-          <Stack direction="row">
-            <Radio value="easy">Easy</Radio>
-            <Radio value="medium">Medium</Radio>
-            <Radio value="hard">Hard</Radio>
-          </Stack>
-        </RadioGroup>
+          <RadioGroup
+            onChange={(nextVal: string) => setDifficulty(nextVal as Difficulty)}
+            value={difficulty}
+          >
+            <Stack direction="row">
+              <Radio value="easy">Easy</Radio>
+              <Radio value="medium">Medium</Radio>
+              <Radio value="hard">Hard</Radio>
+            </Stack>
+          </RadioGroup>
+          <FormLabel>Select question type</FormLabel>
 
-        <RadioGroup
-          onChange={(nextVal: string) =>
-            setQuestionType(nextVal as QuestionType)
-          }
-          value={questionType}
-        >
-          <Stack direction="row">
-            <Radio value="multiple">Multiple Choice</Radio>
-            <Radio value="boolean">True or False</Radio>
-          </Stack>
-        </RadioGroup>
+          <RadioGroup
+            onChange={(nextVal: string) =>
+              setQuestionType(nextVal as QuestionType)
+            }
+            value={questionType}
+          >
+            <Stack direction="row">
+              <Radio value="multiple">Multiple Choice</Radio>
+              <Radio value="boolean">True or False</Radio>
+            </Stack>
+          </RadioGroup>
+          <FormLabel>Select category</FormLabel>
 
-        <Select
-          onChange={(e) => {
-            const categoryInfo: ICategoryInfo = allCategories.filter(
-              (catInfo: ICategoryInfo) => catInfo.name === e.target.value
-            )[0];
-            setSelectedCategoryInfo({
-              id: categoryInfo.id,
-              name: categoryInfo.name,
-            });
-          }}
-          placeholder="Category"
-        >
-          {allCategories.map((data) => (
-            <option key={data.id} value={data.name}>
-              {data.name}
-            </option>
-          ))}
-        </Select>
-        <Button
-          onClick={handleGenerateQuiz}
-          colorScheme="blue"
-          isDisabled={selectedCategoryInfo.name === "" ? true : false}
-        >
-          Begin
-        </Button>
-      </HStack>
+          <Select
+            size="md"
+            onChange={(e) => {
+              const categoryInfo: ICategoryInfo = allCategories.filter(
+                (catInfo: ICategoryInfo) => catInfo.name === e.target.value
+              )[0];
+              setSelectedCategoryInfo({
+                id: categoryInfo.id,
+                name: categoryInfo.name,
+              });
+            }}
+            placeholder="Category"
+          >
+            {allCategories.map((data) => (
+              <option key={data.id} value={data.name}>
+                {data.name}
+              </option>
+            ))}
+          </Select>
+          <Button
+            onClick={handleGenerateQuiz}
+            colorScheme="blue"
+            isDisabled={selectedCategoryInfo.name === "" ? true : false}
+          >
+            Begin
+          </Button>
+        </VStack>
+      </FormControl>
     </>
   );
 }
