15b0b6fdd16347a314f7c437fb6e0d05d6f7f09e
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2024-08-11 13:42:01 +0100

    Updated quiz answers to match new interface (IAnswerInfo)

--- src/SingleQuestion.tsx
+++ src/SingleQuestion.tsx
@@ -1,5 +1,5 @@
 import { Button, SimpleGrid } from "@chakra-ui/react";
-import { IQuestionInfo } from "./types";
+import { IQuestionInfo, IAnswerInfo } from "./types";
 import shuffle from "./shuffleArray";
 import { useEffect, useState } from "react";
 
@@ -16,14 +16,37 @@ function SingleQuestion({
   setSelectedAnswer,
   answerIsSubmitted,
 }: SingleQuestionProps) {
-  const [shuffledAnswers, setShuffledAnswers] = useState<string[]>([]);
+  const [quizAnswers, setQuizAnswers] = useState<IAnswerInfo[]>([]);
 
   useEffect(() => {
-    const shuffled = shuffle([
-      ...questionInfo.incorrect_answers,
+    const allAnswers: string[] = [
       questionInfo.correct_answer,
-    ]);
-    setShuffledAnswers(shuffled);
+      ...questionInfo.incorrect_answers,
+    ];
+
+    const allAnswersWithInfo: IAnswerInfo[] = allAnswers.map(
+      (answerText, idx) => {
+        return {
+          isCorrect: idx === 0 ? true : false,
+          text: answerText,
+        };
+      }
+    );
+
+    // If quiz questions are of type true/false, then sort the array so True is always displayed before False
+    if (allAnswersWithInfo.length === 2) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      allAnswersWithInfo.sort((a, _b) => {
+        if (a.text == "True") return -1;
+        else return 1;
+      });
+    }
+
+    setQuizAnswers(
+      allAnswersWithInfo.length === 2
+        ? allAnswersWithInfo
+        : shuffle(allAnswersWithInfo)
+    );
   }, [questionInfo]);
 
   function handleSelectAnAnswer(idx: number) {
@@ -34,7 +57,7 @@ function SingleQuestion({
     <>
       <h1 className="question">{questionInfo.question}</h1>
       <SimpleGrid columns={2} spacing={10}>
-        {shuffledAnswers.map((ans, idx) => (
+        {quizAnswers.map((ans, idx) => (
           <Button
             key={idx}
             onClick={() => {
@@ -44,7 +67,7 @@ function SingleQuestion({
             variant={selectedAnswer === idx ? "solid" : "outline"}
             isDisabled={answerIsSubmitted}
           >
-            {ans}
+            {ans.text}
           </Button>
         ))}
       </SimpleGrid>
